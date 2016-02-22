3d1f50befb37881e22e3c5ddf327f94efcdfe8aa
TMsosa <tamandamsosa746@gmail.com>
2016-02-21 23:29:09 -0500

    Simple calculator

---
+++ calculation.py
@@ -0,0 +1,30 @@
+print("A: Add")
+print("S: Subtract")
+print("M: Multiply")
+print("D: Div")
+
+select = input("Enter option: ")
+
+if select == "A":
+    num1 = input("num1: ")
+    num2 = input("num2: ")
+    ans = int(num1 + num2)
+    print(ans)
+
+elif select == "S":
+    num1 = input("num1: ")
+    num2 = input("num2: ")
+    ans = int(num1 - num2)
+    print(ans)
+
+elif select == "M":
+    num1 = input("num1: ")
+    num2 = input("num2: ")
+    ans = int(num1 * num2)
+    print(ans)
+
+elif select == "D":
+    num1 = float(input("num1: "))
+    num2 = float(input("num2: "))
+    ans = float(num1 / num2)
+    print(ans)
