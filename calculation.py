f7df017d4c1d1f5c295e0f2829e8a03072c3652a
TMsosa <tamandamsosa746@gmail.com>
2016-02-22 14:20:47 -0500

    Configured calculation cript

--- calculation.py
+++ calculation.py
@@ -8,23 +8,23 @@ select = input("Enter option: ")
 if select == "A":
     num1 = input("num1: ")
     num2 = input("num2: ")
-    ans = int(num1 + num2)
+    ans = int(num1) + int(num2)
     print(ans)
 
 elif select == "S":
     num1 = input("num1: ")
     num2 = input("num2: ")
-    ans = int(num1 - num2)
+    ans = int(num1) - int(num2)
     print(ans)
 
 elif select == "M":
     num1 = input("num1: ")
     num2 = input("num2: ")
-    ans = int(num1 * num2)
+    ans = int(num1) * int(num2)
     print(ans)
 
 elif select == "D":
     num1 = float(input("num1: "))
     num2 = float(input("num2: "))
-    ans = float(num1 / num2)
+    ans = float(num1) / float(num2)
     print(ans)
